4fc0bb391fc35911573ae89fd4edacb1af9447c9
Arulkumar <aruljagan8124@gmail.com>
2026-07-06 14:23:09 +0530

    updated

---
+++ react/react/node/Day3/component/src/components/Home.jsx
@@ -0,0 +1,24 @@
+
+
+const Home = ()=>{
+    return (
+        <>
+        
+        <div className="flexbox">
+            <h1>MyPage</h1>
+            <div className="navbar">
+                <p>Home</p>
+                <p>Main</p>
+                <p>Contact</p>
+                <p>About</p>
+            </div>
+            </div>
+
+            <div>
+                <img src="" alt="" />
+            </div>
+        </>
+    )
+}
+
+export default Home
